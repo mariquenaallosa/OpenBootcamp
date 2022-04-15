90b95a991324a5d7d7b87cdd2c0186ca30388e11
mariquenaallosa <allosamariquena@gmail.com>
2022-04-15 16:18:17 -0300

    while y do while

--- Ejercicio tema 4_sentencias de control/Main.java
+++ Ejercicio tema 4_sentencias de control/Main.java
@@ -4,7 +4,7 @@ public class Main {
         int numeroif = 0;
 
         if (numeroif==0) {
-            System.out.println("El número "+ numeroif +" es igual a 0");
+            System.out.println("Es igual a 0");
         } else if(numeroif<=0) { 
             System.out.println("El número "+ numeroif +" es negativo");
         } else if (numeroif>=0) {
@@ -12,6 +12,20 @@ public class Main {
         }
 
 
+    // Crea un bucle While, este bucle tendrá que tener como condición que la variable numeroWhile sea inferior a 3, el bloque de código que tendrá el bucle deberá:
+    // Incrementar el valor de la variable en uno cada vez que se ejecute.
+    // Mostrarlo por pantalla cada vez que se ejecute.
+        int numeroWhile = 0;
+        while (numeroWhile < 3){
+        System.out.println(numeroWhile);
+        numeroWhile++;
+        }
         
-    }
+    // Para el bucle Do While, deberás crear la misma estructura que en el While, pero solo se debe ejecutar una vez.
+        int numeroDoWhile = 0;
+        do{
+            System.out.println(numeroDoWhile);
+            numeroDoWhile++;
+        } while (numeroDoWhile < 3);
+}
 }
