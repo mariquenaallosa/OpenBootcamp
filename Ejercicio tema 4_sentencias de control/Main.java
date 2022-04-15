e6dd32332f1d864301c45d8c104558968e7c3645
mariquenaallosa <allosamariquena@gmail.com>
2022-04-15 16:08:54 -0300

    Usando if

---
+++ Ejercicio tema 4_sentencias de control/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+    // Usando un if, crear una condición que compare si la variable numeroIf es positivo, negativo, o 0
+        int numeroif = 0;
+
+        if (numeroif==0) {
+            System.out.println("El número "+ numeroif +" es igual a 0");
+        } else if(numeroif<=0) { 
+            System.out.println("El número "+ numeroif +" es negativo");
+        } else if (numeroif>=0) {
+            System.out.println("El número "+ numeroif +" es positivo");
+        }
+
+
+        
+    }
+}
